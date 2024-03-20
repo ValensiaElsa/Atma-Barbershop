d9ebaef83e3504e83abe0da142613e3276b6ec76
ValensiaElsa <221711705@students.uajy.ac.id>
2024-03-20 13:12:14 +0700

    p commit

--- app/dashboard/reservations/page.tsx
+++ app/dashboard/reservations/page.tsx
@@ -1,7 +1,14 @@
-import ReservationsTable from '@/app/ui/reservations/table';
-export default function Page() {
+import Pagination from '@/app/ui/reservations/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/reservations/table';
+import { CreateReservation } from '@/app/ui/reservations/buttons';
+import { kanit, lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+ 
+export default async function Page() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <div className="w-full">
       <p>
         Reservation Pages
       </p>
@@ -11,7 +18,16 @@ export default function Page() {
       <p>
         Valensia Elsa Kurnia
       </p>
-      <ReservationsTable query="2024-06-05" currentPage={1} />
-    </main>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search reservations..." />
+        <CreateReservation />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
   );
 }
