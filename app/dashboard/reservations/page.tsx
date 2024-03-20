f246c14d88c605a21230e1762031848d534f07fe
ValensiaElsa <221711705@students.uajy.ac.id>
2024-03-20 12:40:57 +0700

    ugd 3

--- app/dashboard/reservations/page.tsx
+++ app/dashboard/reservations/page.tsx
@@ -1,3 +1,4 @@
+import ReservationsTable from '@/app/ui/reservations/table';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col">
@@ -10,6 +11,7 @@ export default function Page() {
       <p>
         Valensia Elsa Kurnia
       </p>
+      <ReservationsTable query="2024-06-05" currentPage={1} />
     </main>
   );
 }
